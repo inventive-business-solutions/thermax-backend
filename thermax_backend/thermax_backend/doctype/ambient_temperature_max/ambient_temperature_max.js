cc9dd02da97c1231c8970b984c3c02386822f9d0
Rishi-Inventive <Rishi.Khandagle@inventivebizsol.com>
2024-09-26 16:42:08 +0530

    feature/adding project information dropdown doctype and project creation

---
+++ thermax_backend/thermax_backend/doctype/ambient_temperature_max/ambient_temperature_max.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhishek Bankar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ambient Temperature Max", {
+// 	refresh(frm) {
+
+// 	},
+// });
