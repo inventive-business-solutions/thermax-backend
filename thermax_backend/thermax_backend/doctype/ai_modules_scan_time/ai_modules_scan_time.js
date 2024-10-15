308e80b871b59b50618f2e2a9b211db36ad6ec4b
Rishi-Inventive <Rishi.Khandagle@inventivebizsol.com>
2024-10-15 12:50:35 +0530

    Adding data of Design Basis into Doctypes and Taking pull

---
+++ thermax_backend/thermax_backend/doctype/ai_modules_scan_time/ai_modules_scan_time.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhishek Bankar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AI Modules Scan Time", {
+// 	refresh(frm) {
+
+// 	},
+// });
