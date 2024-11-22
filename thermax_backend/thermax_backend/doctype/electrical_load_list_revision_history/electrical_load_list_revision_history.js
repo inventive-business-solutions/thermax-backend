1e7b14b51e24457dcbeaa80248db9817979f7076
Abhishek Bankar <abhishekraje30@gmail.com>
2024-11-22 17:09:43 +0530

    load list apis added

---
+++ thermax_backend/thermax_backend/doctype/electrical_load_list_revision_history/electrical_load_list_revision_history.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhishek Bankar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Electrical Load List Revision History", {
+// 	refresh(frm) {
+
+// 	},
+// });
