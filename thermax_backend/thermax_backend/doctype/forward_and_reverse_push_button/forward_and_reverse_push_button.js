7b2b3922478ee233f1305e858c8c20e3d8b53af5
Rishi-Inventive <rishi.khandagle@inventivebizsol.com>
2024-12-27 14:29:37 +0000

    Design Basis Changes backend

---
+++ thermax_backend/thermax_backend/doctype/forward_and_reverse_push_button/forward_and_reverse_push_button.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhishek Bankar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Forward and Reverse Push Button", {
+// 	refresh(frm) {
+
+// 	},
+// });
