4b39b2dc60e772f8c4c6ea6a0b627f6e10a147ef
Rishi-Inventive <rishi.khandagle@inventivebizsol.com>
2025-02-01 09:03:23 +0000

    taking pull of uploaded dedesign basis excels

--- thermax_backend/thermax_backend/doctype/design_basis_revision_history/api.py
+++ thermax_backend/thermax_backend/doctype/design_basis_revision_history/api.py
@@ -155,60 +155,60 @@ def get_design_basis_excel():
     cover_sheet = create_cover_sheet(
         cover_sheet, project_data, revision_data, division_name
     )
-    design_basis_sheet = get_design_basis_sheet(
-        design_basis_sheet=design_basis_sheet,
-        project_id=project_id,
-        revision_id=revision_id,
-        division_name=division_name,
-        make_of_components_data=make_of_components_data,
-    )
+    # design_basis_sheet = get_design_basis_sheet(
+    #     design_basis_sheet=design_basis_sheet,
+    #     project_id=project_id,
+    #     revision_id=revision_id,
+    #     division_name=division_name,
+    #     make_of_components_data=make_of_components_data,
+    # )
 
-    if division_name == "Enviro" or division_name == "WWS IPG":
-        template_workbook = get_enviro_ipg_db_excel(
-            template_workbook=template_workbook,
-            mcc_sheet=mcc_sheet,
-            pcc_sheet=pcc_sheet,
-            mcc_cum_plc_sheet=mcc_cum_plc_sheet,
-            project_data=project_data,
-            make_of_components_data=make_of_components_data,
-            revision_id=revision_id,
-        )
-    elif division_name == "Heating":
-        template_workbook = get_heating_db_excel(
-            template_workbook=template_workbook,
-            mcc_sheet=mcc_sheet,
-            pcc_sheet=pcc_sheet,
-            mcc_cum_plc_sheet=mcc_cum_plc_sheet,
-            project_data=project_data,
-            make_of_components_data=make_of_components_data,
-            revision_id=revision_id,
-        )
-    elif division_name == "WWS SPG" or division_name == "WWS Services":
-        template_workbook = get_wws_services_spg_db_excel(
-            template_workbook=template_workbook,
-            mcc_sheet=mcc_sheet,
-            pcc_sheet=pcc_sheet,
-            mcc_cum_plc_sheet=mcc_cum_plc_sheet,
-            project_data=project_data,
-            make_of_components_data=make_of_components_data,
-            revision_id=revision_id,
-        )
-    else:
-        template_workbook = get_heating_db_excel(
-            template_workbook=template_workbook,
-            mcc_sheet=mcc_sheet,
-            pcc_sheet=pcc_sheet,
-            mcc_cum_plc_sheet=mcc_cum_plc_sheet,
-            project_data=project_data,
-            make_of_components_data=make_of_components_data,
-            revision_id=revision_id,
-        )
+    # if division_name == "Enviro" or division_name == "WWS IPG":
+    #     template_workbook = get_enviro_ipg_db_excel(
+    #         template_workbook=template_workbook,
+    #         mcc_sheet=mcc_sheet,
+    #         pcc_sheet=pcc_sheet,
+    #         mcc_cum_plc_sheet=mcc_cum_plc_sheet,
+    #         project_data=project_data,
+    #         make_of_components_data=make_of_components_data,
+    #         revision_id=revision_id,
+    #     )
+    # elif division_name == "Heating":
+    #     template_workbook = get_heating_db_excel(
+    #         template_workbook=template_workbook,
+    #         mcc_sheet=mcc_sheet,
+    #         pcc_sheet=pcc_sheet,
+    #         mcc_cum_plc_sheet=mcc_cum_plc_sheet,
+    #         project_data=project_data,
+    #         make_of_components_data=make_of_components_data,
+    #         revision_id=revision_id,
+    #     )
+    # elif division_name == "WWS SPG" or division_name == "WWS Services":
+    #     template_workbook = get_wws_services_spg_db_excel(
+    #         template_workbook=template_workbook,
+    #         mcc_sheet=mcc_sheet,
+    #         pcc_sheet=pcc_sheet,
+    #         mcc_cum_plc_sheet=mcc_cum_plc_sheet,
+    #         project_data=project_data,
+    #         make_of_components_data=make_of_components_data,
+    #         revision_id=revision_id,
+    #     )
+    # else:
+    #     template_workbook = get_heating_db_excel(
+    #         template_workbook=template_workbook,
+    #         mcc_sheet=mcc_sheet,
+    #         pcc_sheet=pcc_sheet,
+    #         mcc_cum_plc_sheet=mcc_cum_plc_sheet,
+    #         project_data=project_data,
+    #         make_of_components_data=make_of_components_data,
+    #         revision_id=revision_id,
+    #     )
 
-        # MCC
+    #     # MCC
 
-    template_workbook.remove(mcc_sheet)
-    template_workbook.remove(pcc_sheet)
-    template_workbook.remove(mcc_cum_plc_sheet)
+    # template_workbook.remove(mcc_sheet)
+    # template_workbook.remove(pcc_sheet)
+    # template_workbook.remove(mcc_cum_plc_sheet)
 
     # Load the workbook from the template path
     # template_workbook.save("design_basis.xlsx")
